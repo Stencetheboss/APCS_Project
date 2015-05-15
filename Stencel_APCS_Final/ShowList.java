88f24696bb4f5eb53c0b1c840f0537aa22118ff2
stencetheboss <andrewstencel333@gmail.com>
2015-05-15 08:37:15 -0500

    made rough GUI objects

---
+++ Stencel_APCS_Final/ShowList.java
@@ -0,0 +1,18 @@
+
+/**
+ * Write a description of class ShowList here.
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
+ */
+public class ShowList
+{
+
+    public static void main (String[] args)
+    {
+        QueueGUI queue1 = new QueueGUI();
+        queue1.display();
+        WatchedGUI watched1 = new WatchedGUI();
+        watched1.display();
+    }
+}
